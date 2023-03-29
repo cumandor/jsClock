129bb3871df2ba3fb67012fe39bd70ecbff0a53e
Misha Volvoik <volovimihail@gmail.com>
2023-03-29 23:04:12 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const hr = document.querySelector('#hr');
+const min = document.querySelector('#min');
+const sec = document.querySelector('#sec');
+const degree = 6;
+
+setInterval(() => {
+
+    const date = new Date();
+    const hh = date.getHours() * 30;
+    const mm = date.getMinutes() * degree;
+    const ss = date.getSeconds() * degree;
+
+    hr.style.transform = `rotateZ(${hh + (mm / 12)}deg)`;
+    min.style.transform = `rotateZ(${mm}deg)`;
+    sec.style.transform = `rotateZ(${ss}deg)`;
+});
